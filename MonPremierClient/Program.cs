02d70d1cc358026f24d451cd13caa215ef25108e
JLiardet <jliardet@swissriskcare.ch>
2021-01-17 13:37:52 +0100

    Add project files.

---
+++ MonPremierClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonPremierClient
+{
+    class Program
+    {
+        static async void MaMethodeAsynchrone(string input, ServiceReference1.Service1Client client)
+        {
+            Console.WriteLine($"Résultat pour {input}: { await client.GetDataAsync(Convert.ToInt32(input))}");
+        }
+
+        static void Main(string[] args)
+        {
+            var input = "";
+
+            var client = new ServiceReference1.Service1Client();
+
+            do
+            {
+                Console.WriteLine("Entrez un nombre");
+
+                input = Console.ReadLine();
+
+                Task.Run(() => MaMethodeAsynchrone(input, client));
+
+            } while (input != "100");
+
+            client.Close();
+;        }
+    }
+}
