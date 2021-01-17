4350af1d06bcaabce0ff9f800b0952d0b17bae15
JLiardet <jliardet@swissriskcare.ch>
2021-01-17 15:14:14 +0100

    tes3

--- MonPremierServiceWCF/IService1.cs
+++ MonPremierServiceWCF/IService1.cs
@@ -28,7 +28,7 @@ namespace MonPremierServiceWCF
     public class CompositeType
     {
         bool boolValue = true;
-        string stringValue = "Hello Word 1";
+        string stringValue = "Hello Word 2";
 
         [DataMember]
         public bool BoolValue
